9fc422ea8377c7536f31b9704724fbfdf56f341f
Aldrin Mathew <admin@aldrinmathew.com>
2024-08-13 12:03:04 +0530

    Added `Result<T, E>` datatype
    - Added datatype that represents a resultant of some operation. It is
      a generic datatype
    
    - The default error type is `VkMiniError` but that can be changed

--- vkmini.hpp
+++ vkmini.hpp
@@ -1,7 +1,13 @@
 #ifndef VK_HPP
 #define VK_HPP
 
+#include <functional>
+#include <mutex>
 #include <optional>
+#include <thread>
+#include <utility>
+#include <variant>
+#include <vector>
 #include <vulkan/vulkan_core.h>
 
 #define use [[nodiscard]]
@@ -35,6 +41,31 @@ struct ErrorDetail {
 
   use bool is_ok() const { return vkMini == VKMINI_NO_ERROR; }
   use operator bool() const { return is_ok(); }
+};
+
+template <typename T, typename E = VkMiniError> struct Result {
+  std::variant<T, E> data;
+
+  Result(std::optional<T> value, std::optional<E> error)
+      : data(value.has_value()
+                 ? std::variant<T, E>(std::in_place_index<0>, value.value())
+                 : std::variant<T, E>(std::in_place_index<1>, error.value())) {}
+
+public:
+  use static Result Ok(T value) { return Result(value, None); }
+  use static Result Error(E error) { return Result(None, error); }
+
+  use bool is_ok() { return data.index() == 0; }
+  use T get_value() && { return std::get<T>(data); }
+  use T &get_value() & { return std::get<T>(data); }
+  use T const &get_value() const & { return std::get<T>(data); }
+
+  use bool is_error() { return !is_ok(); }
+  use E get_error() && { return std::get<E>(data); }
+  use E &get_error() & { return std::get<E>(data); }
+  use E const &get_error() const & { return std::get<E>(data); }
+
+  use operator bool() { return is_ok(); }
 };
 
 } // namespace vk
