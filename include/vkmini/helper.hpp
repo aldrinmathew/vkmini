8ad417f611f6137002dde5e2e6ccfb1f39617a5e
Aldrin Mathew <admin@aldrinmathew.com>
2024-10-18 23:17:28 +0530

    Added helpful type definitions in helper.hpp
    - Added common aliases I use as type definitions in helper.hpp
    
    - These types are meant to be used throughout the codebase

--- include/vkmini/helper.hpp
+++ include/vkmini/helper.hpp
@@ -14,32 +14,46 @@
 #else
 #define VKMINI_IF_MULTITHREAD(x)
 #define VKMINI_INSIDE_LOCK(x) x
+#include <mutex>
 #endif
+
+#include <boost/assert/source_location.hpp>
+#include <boost/stacktrace/stacktrace.hpp>
 #include <cstdint>
 #include <filesystem>
+#include <iostream>
 #include <map>
 #include <optional>
+#include <set>
 #include <string>
 #include <vector>
+
+#define use [[nodiscard]]
 
 namespace vk {
 
 #define use [[nodiscard]]
 
-using u8     = uint8_t;
-using u16    = uint16_t;
-using u32    = uint32_t;
-using u64    = uint64_t;
-using i32    = int32_t;
-using i64    = int64_t;
-using uchar  = unsigned char;
-using String = std::string;
-using usize  = std::size_t;
-using Path   = std::filesystem::path;
+using u8         = uint8_t;
+using u16        = uint16_t;
+using u32        = uint32_t;
+using u64        = uint64_t;
+using i8         = int8_t;
+using i16        = int16_t;
+using i32        = int32_t;
+using i64        = int64_t;
+using uchar      = unsigned char;
+using String     = std::string;
+using StringView = std::string_view;
+using usize      = std::size_t;
+using Path       = std::filesystem::path;
 
-template <typename T> using Maybe           = std::optional<T>;
-template <typename T> using Vec             = std::vector<T>;
-template <typename A, typename B> using Map = std::map<A, B>;
+template <typename T> using Maybe = std::optional<T>;
+template <typename T> using Vec   = std::vector<T>;
+template <typename T> using Set   = std::set<T>;
+
+template <typename A, typename B> using Map  = std::map<A, B>;
+template <typename A, typename B> using Pair = std::pair<A, B>;
 
 static const auto None = std::nullopt;
 
